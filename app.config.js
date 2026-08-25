f582027df5f892e7fe0461dbdc2310928685ba56
neuralnex <omezirizion@gmail.com>
2026-08-25 05:02:39 +0100

    push fixes for keys

--- app.config.js
+++ app.config.js
@@ -1,18 +1,9 @@
 module.exports = ({ config }) => {
   const extra = {
     ...(config.extra || {}),
-    privyAppId:
-      process.env.PRIVY_APP_ID ||
-      process.env.EXPO_PUBLIC_PRIVY_APP_ID ||
-      config.extra?.privyAppId ||
-      '',
-    privyClientId:
-      process.env.PRIVY_CLIENT_ID ||
-      process.env.EXPO_PUBLIC_PRIVY_CLIENT_ID ||
-      config.extra?.privyClientId ||
-      '',
-    adminOnly:
-      (process.env.ADMIN_ONLY === 'true') || config.extra?.adminOnly || false,
+    privyAppId: process.env.EXPO_PUBLIC_PRIVY_APP_ID || config.extra?.privyAppId || '',
+    privyClientId: process.env.EXPO_PUBLIC_PRIVY_CLIENT_ID || config.extra?.privyClientId || '',
+    adminOnly: (process.env.EXPO_PUBLIC_ADMIN_ONLY === 'true') || config.extra?.adminOnly || false,
   };
 
   return {
